1dd8b4e33b8128afdc115465d70db4108828d7d2
Minhal <minhanadeem@gmail.com>
2024-12-11 22:46:53 +0500

    Bloc app with notes

--- lib/main.dart
+++ lib/main.dart
@@ -16,23 +16,9 @@ void main() {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: BlocProvider(
-        create: (_) => PersonBloc(),
-        child: const MyHomePage(title: 'Flutter Demo Home Page'),
-      ),
+      home: MyHomePage(title: 'Bloc App Notes'),
     ),
   );
-}
-
-Future<Iterable<Person>> getPersons(String url) => HttpClient()
-    .getUrl(Uri.parse(url))
-    .then((req) => req.close())
-    .then((resp) => resp.transform(utf8.decoder).join())
-    .then((str) => json.decode(str) as List<dynamic>)
-    .then((list) => list.map((e) => Person.fromJson(e)));
-
-extension Subscript<T> on Iterable<T> {
-  T? operator [](int index) => length > index ? elementAt(index) : null;
 }
 
 class MyHomePage extends StatelessWidget {
@@ -46,58 +32,7 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              TextButton(
-                onPressed: () {
-                  context.read<PersonBloc>().add(
-                        const LoadPersonAction(
-                          url: PersonUrl.persons1,
-                        ),
-                      );
-                },
-                child: Text("Load json #1"),
-              ),
-              TextButton(
-                onPressed: () {
-                  context.read<PersonBloc>().add(
-                        const LoadPersonAction(
-                          url: PersonUrl.persons2,
-                        ),
-                      );
-                },
-                child: Text("Load json #2"),
-              ),
-            ],
-          ),
-          BlocBuilder<PersonBloc, FetchResult?>(
-            buildWhen: (previous, current) {
-              return previous?.persons != current?.persons;
-            },
-            builder: (context, fetchResult) {
-              fetchResult?.log();
-              final persons = fetchResult?.persons;
-              if (persons == null) {
-                return const SizedBox();
-              }
-
-              return Expanded(
-                child: ListView.builder(
-                  itemBuilder: (context, index) {
-                    final person = persons[index];
-
-                    return ListTile(
-                      title: Text(person?.name ?? ""),
-                    );
-                  },
-                ),
-              );
-            },
-          )
-        ],
-      ),
+      body: Text("data"),
     );
   }
 }
